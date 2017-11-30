a8abe5555098c8c5196a2ec3512902337832fd77
Harrison <harrisoncurtis96@gmail.com>
2017-11-30 12:36:48 +0000

    add data to problem table and caller/employee table

--- sql_statements.sql
+++ sql_statements.sql
@@ -20,11 +20,25 @@ INSERT INTO Problem Type (Problem_Type_Name, Specialist_ID, Counter) VALUES
 /* Operator Table */
 
 /* Problem Table */
-
+INSERT INTO Problem_Table
+(Operator_ID, Specialist_ID, Caller_ID, Close_Date) VALUES
+('12', '2452', '3543', '13/10/17')
+('13', '2453', '6534', '15/10/17')
+('72', '2452', '1232', '16/11/17')
+('15', '2455', '123', '15/02/12')
+('12', '2456', '987', '27/04/16')
+('13', '2457', '9876', '01/12/17')
 /* Software Table */
 
 /* Caller/Employee Table */
-
+INSERT INTO Caller_Employee_Table
+(Caller_ID, Caller_Name, Job, Department, Extension) VALUES
+('3543', 'Alex Jones', 'Customer Assitant', 'Customer_Services', '012')
+('6534', 'Ranaplh', 'Manager', 'Logistics', '013')
+('1232', 'Rishi', 'Clerk', 'Human_Resources', '014')
+('123', 'Punjab', 'Accountant', 'Finance', '015')
+('987', 'Posco', 'Foreman', 'Logistics', '013')
+('9876', 'xiaohu', 'Operator', 'Customer_Service', '016')
 /* Problem Status Table */
 
 /* Equipment Table */

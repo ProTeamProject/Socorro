f89ff9773cc1cec0701804d3830c3ce65ddb8f67
ollypolly <o.pethick-16@student.lboro.ac.uk>
2017-11-30 12:21:31 +0000

    add sql statement file

---
+++ sql_statements.sql
@@ -0,0 +1,24 @@
+/* Account Table */
+
+/* Specialist Table */
+INSERT INTO Specialist (Account_ID, Specialist_Name, Average_Time, Number_Solved, Busy) VALUES
+('2452', 'Janet', '5.2', '23', 'False'),
+('2453', 'Bossman', '2.3', '42', 'True'),
+('2454', 'Brad', '0.8', '8', 'True'),
+('2455', 'Donald', '4.2', '64', 'False'),
+('2456', 'Aaron', '6.7', '52', 'True'),
+('2457', 'Edna', '2.3', '12', 'False');
+
+/* Problem Type Table */
+
+/* Operator Table */
+
+/* Problem Table */
+
+/* Software Table */
+
+/* Caller/Employee Table */
+
+/* Problem Status Table */
+
+/* Equipment Table */

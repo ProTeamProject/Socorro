10b91cb4286a6249de746c27adc8cbd14891ee17
Aaron Homes <aaronhomes6@gmail.com>
2017-12-10 20:59:40 +0000

    Added SQL Statements

--- sql_statements.sql
+++ sql_statements.sql
@@ -1,31 +1,51 @@
-/* Account Table */
+/* Account Table: Joshua Hope*/
 
-/* Specialist Table */
+/* Specialist Table: Harrison Curtis */
 
-/* Problem Table */
+/* Problem Table: Aaron Homes*/
 
-/* Software Problem Table */
+INSERT INTO Problem Table(Problem_ID, Account_ID, Account_ID, Caller_ID, Close_Date)
+Values('1','12','2452','3543','13/10/17'),
+      ('2','13','2453','6524','15/10/17'),
+      ('3','72','2454','1232','16/11/17'),
+      ('4','15','2455','123','15/02/12'),
+      ('5','12','2456','987','27/04/16'),
+      ('6','13','2457','9876','01/12/17');
 
-/* Status Table */
+/* Software Problem Table: Brad Reeder*/
 
-/* Specialises In Table */
+/* Status Table: Harry Salmon*/
 
-/* Problem Status Table */
+/* Specialises In Table: Joshua Hope*/
 
-/* Equipment Register Table */
+/* Problem Status Table: Harrison Curtis */
 
-/* Problem Counter Table */
+/* Equipment Register Table: Aaron Homes*/
 
-/* Problem Type Table */
+INSERT INTO Equipment Register Table(Equipment_Name, Equipment_Make, Equipment_Type)
+Values('Dell GTX 12','Dell','Computer'),
+      ('Macbook Pro','Apple','Laptop'),
+      ('Sony Mouse','Sony','Peripheral'),
+      ('Epson 23x43','Epson','Peripheral'),
+      ('Acer Predator','Acer','Laptop');
 
-/* Operator Table */
+/* Problem Counter Table: Brad Reeder*/
 
-/* Software Table */
+/* Problem Type Table: Harry Salmon*/
 
-/* Software Table */
+/* Operator Table: Joshua Hope*/
 
-/* Employee Table */
+/* Software Table: Harrison Curtis*/
 
-/* Equipment Table */
+/* Employee Table: Aaron Homes */
 
-/* Equipment Problem Table */
+INSERT INTO Employee Table(Caller_ID, Caller_Name, Job, Department, Extension)
+Values('3543','Alex Jones','Customer Assistant','Customer Services','12'),
+      ('6534','Ranalph','Manager','Logistics','12'),
+      ('1232','Rishi','Clerk','Human Resources','12'),
+      ('123','Punjab','Accountant','Finance','12'),
+      ('987','Posco','Foreman','Logistics','12');
+
+/* Equipment Table: Brad Reeder*/
+
+/* Equipment Problem Table: Harry Salmon*/

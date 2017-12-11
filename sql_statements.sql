8a8836d6b2c13ee1c997a87d3f1c87451cc8f007
JoshuaJamesHope <33814325+JoshuaJamesHope@users.noreply.github.com>
2017-12-11 12:16:44 +0000

    update Sql

--- sql_statements.sql
+++ sql_statements.sql
@@ -1,4 +1,15 @@
 /* Account Table: Joshua Hope*/
+INSERT INTO Account_Table (Account_ID, Name, Username, Password,Job Type)
+VALUES ('2312',	'Alice',	'aliceSmith',	'K2p98h23',	'Operator'),
+('2313',	'Janet',	'janetRodgers',	'j4V92391',	'Specailist'),
+('2314',	'Bossman',	'mrBossman',	'5tS99a82',	'Specailist'),
+('2315',	'Olly', 'ollyPethick',	'cW18305P',	'Operator'),
+('2316',	'Brad',	'bradReeder',	'321W9rT1',	'specailist'),
+('2317',	'Donald	donaldTrump',	'w93P44G7',	'specailist'),
+('2318',	'Aaron',	'aaronHomes',	'bS4535h9',	'specailist'),
+('2319',	'Joshua'	,'joshuaHope',	'39l5IJ90',	'Operator'),
+('2320'	,'Edna',	'ednaMode',	'tQ52224c',	'specailist'),
+('2321',	'Harrison',	'harrisonCurtis',	'0b67P9g1',	'operator')
 
 /* Specialist Table: Harrison Curtis */
 
@@ -17,6 +28,12 @@ Values('1','12','2452','3543','13/10/17'),
 /* Status Table: Harry Salmon*/
 
 /* Specialises In Table: Joshua Hope*/
+INSERT INTO Specailist_In (Problem_Type_ID, Account_ID)
+VALUES('1','12'),
+('2',	'13'),
+('3',	'72'),
+('4',	'15'),
+('5',	'12')
 
 /* Problem Status Table: Harrison Curtis */
 
@@ -34,6 +51,12 @@ Values('Dell GTX 12','Dell','Computer'),
 /* Problem Type Table: Harry Salmon*/
 
 /* Operator Table: Joshua Hope*/
+INSERT INTO Operator_Table
+VALUES ('2312',	'4'),
+('2315',	'7'),
+('2319',	'9'),
+('2321',	'3')
+
 
 /* Software Table: Harrison Curtis*/
 

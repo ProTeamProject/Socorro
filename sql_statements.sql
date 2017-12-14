b0ab7617639f981ac73633adf3bdeadf90db12c6
ollypolly <o.pethick-16@student.lboro.ac.uk>
2017-12-14 14:35:13 +0000

    update table names and format

--- sql_statements.sql
+++ sql_statements.sql
@@ -1,6 +1,7 @@
 /* Account Table: Joshua Hope*/
-INSERT INTO Account_Table (Account_ID, Name, Username, Password,Job Type)
-VALUES ('2312',	'Alice',	'aliceSmith',	'K2p98h23',	'Operator'),
+INSERT INTO Account (Account_ID, Name, Username, Password, Job_Type)
+VALUES
+('2312',	'Alice',	'aliceSmith',	'K2p98h23',	'Operator'),
 ('2313',	'Janet',	'janetRodgers',	'j4V92391',	'Specailist'),
 ('2314',	'Bossman',	'mrBossman',	'5tS99a82',	'Specailist'),
 ('2315',	'Olly', 'ollyPethick',	'cW18305P',	'Operator'),
@@ -12,8 +13,7 @@ VALUES ('2312',	'Alice',	'aliceSmith',	'K2p98h23',	'Operator'),
 ('2321',	'Harrison',	'harrisonCurtis',	'0b67P9g1',	'operator')
 
 /* Specialist Table: Harrison Curtis */
-INSERT INTO Specialist_Table
-(Account_ID, Average_Time, Problems_Assigned, Busy)
+INSERT INTO Specialist (Account_ID, Average_Time, Problems_Assigned, Busy)
 VALUES
 ('2313', '5.2', '23', '3', 'FALSE'),
 ('2314', '2.3', '42', '2', 'TRUE'),
@@ -23,41 +23,35 @@ VALUES
 ('2320', '2.3', '12', '2'. 'FALSE'),
 
 /* Problem Table: Aaron Homes*/
-
-INSERT INTO Problem Table(Problem_ID, Account_ID, Account_ID, Caller_ID, Close_Date)
-Values('1','12','2452','3543','13/10/17'),
-      ('2','13','2453','6524','15/10/17'),
-      ('3','72','2454','1232','16/11/17'),
-      ('4','15','2455','123','15/02/12'),
-      ('5','12','2456','987','27/04/16'),
-      ('6','13','2457','9876','01/12/17');
+INSERT INTO Problem (Problem_ID, Account_ID, Account_ID, Caller_ID, Close_Date)
+VALUES
+('1','12','2452','3543','13/10/17'),
+('2','13','2453','6524','15/10/17'),
+('3','72','2454','1232','16/11/17'),
+('4','15','2455','123','15/02/12'),
+('5','12','2456','987','27/04/16'),
+('6','13','2457','9876','01/12/17');
 
 /* Software Problem Table: Brad Reeder*/
-INSERT INTO Software_Problem Table (Problem_ID, Software_License_Number, Operating_System, Affected_Hardware)
-Values('1', '2738918ff2873646','Windows 10','Computer'),
-	  ('2','48857845asdgA4','Macintosh','Laptop'),
-	  ('3','aff141fda511','Linux','Computer'),
-	  ('4','gijsgpaj142dna','Macintosh','Laptop'),
-	  ('5','dagjkndaogndao12','Windows 10','Laptop'),
-	  ('5','ignrierfbis21ngona','Windows 10','Computer')
+INSERT INTO Software_Problem (Problem_ID, Software_License_Number, Operating_System, Affected_Hardware)
+Values
+('1', '2738918ff2873646','Windows 10','Computer'),
+('2','48857845asdgA4','Macintosh','Laptop'),
+('3','aff141fda511','Linux','Computer'),
+('4','gijsgpaj142dna','Macintosh','Laptop'),
+('5','dagjkndaogndao12','Windows 10','Laptop'),
+('5','ignrierfbis21ngona','Windows 10','Computer')
 
 /* Status Table: Harry Salmon*/
-INSERT INTO Status (Status ID, Status Type)
+INSERT INTO Status (Status_ID, Status_Type)
 VALUES ('134', 'call'),
 ('135', 'open'),
 ('136', 'close'),
 ('137', 'assign'),
 ('138', 'note');
-/* Specialises In Table joshua */
-INSERT INTO Specailist_In (Problem_Type_ID, Account_ID)
-VALUES('1','12'),
-('2',	'13'),
-('3',	'72'),
-('4',	'15'),
-('5',	'12')
 
-/* Specialises In Table: Joshua Hope*/
-INSERT INTO Specailist_In (Problem_Type_ID, Account_ID)
+/* Problem_Type_Specialist Table: Joshua Hope*/
+INSERT INTO Problem_Type_Specialist (Problem_Type_ID, Account_ID)
 VALUES('1','12'),
 ('2',	'13'),
 ('3',	'72'),
@@ -65,45 +59,46 @@ VALUES('1','12'),
 ('5',	'12')
 
 /* Problem Status Table: Harrison Curtis - HS redone */
-INSERT INTO Problem Status (Problem ID, Comment, Commenter, status date)
-VALUES ('1', 'loss of connection', '2312', '11/2/1'),
+INSERT INTO Problem_Status (Problem_ID, Comment, Commenter, Status_Date)
+VALUES
+('1', 'loss of connection', '2312', '11/2/1'),
 ('2', 'help needed formating', '2315', '11/3/17'),
 ('3', 'a virus has been detected', '2319', '11/3/17'),
 ('4', 'paper jam', '2321', '12/3/17'),
 ('5', 'emails not send', '2321', '12/3/17'),
 ('6', 'loss of email', '2312', '13/3/17');
 
-
-
-
-/* Operator Table joshua */
-INSERT INTO Operator_Table(Account_ID, Number_Calls)
-VALUES ('2312',	'4'),
+/* Operator Table Joshua */
+INSERT INTO Operator (Account_ID, Number_Calls)
+VALUES
+('2312',	'4'),
 ('2315',	'7'),
 ('2319',	'9'),
 ('2321',	'3')
 
 /* Equipment Register Table: Aaron Homes*/
-INSERT INTO Equipment Register Table(Equipment_Name, Equipment_Make, Equipment_Type)
-Values('Dell GTX 12','Dell','Computer'),
-      ('Macbook Pro','Apple','Laptop'),
-      ('Sony Mouse','Sony','Peripheral'),
-      ('Epson 23x43','Epson','Peripheral'),
-      ('Acer Predator','Acer','Laptop');
+INSERT INTO Equipment_Register (Equipment_Name, Equipment_Make, Equipment_Type)
+VALUES
+('Dell GTX 12','Dell','Computer'),
+('Macbook Pro','Apple','Laptop'),
+('Sony Mouse','Sony','Peripheral'),
+('Epson 23x43','Epson','Peripheral'),
+('Acer Predator','Acer','Laptop');
 
 
 /* Problem Counter Table: Brad Reeder*/
-INSERT INTO Problem Counter Table (Problem_Type_ID, Occurences)
-VALUES ('1','142')
-	   ('2','124')
-	   ('3','152')
-	   ('4','612')
-	   ('5','532')
-
+INSERT INTO Problem_Counter (Problem_Type_ID, Occurences)
+VALUES
+('1','142')
+('2','124')
+('3','152')
+('4','612')
+('5','532')
 
 /* Problem Type Table: Harry Salmon*/
-INSERT INTO Problem Type (Problem_Type_ID, Problem_Type_Name)
-VALUES ('1', 'networking'),
+INSERT INTO Problem_Type (Problem_Type_ID, Problem_Type_Name)
+VALUES
+('1', 'networking'),
 ('2', 'security'),
 ('3', 'email'),
 ('4', 'word processing'),
@@ -111,15 +106,14 @@ VALUES ('1', 'networking'),
 
 /* Operator Table: Joshua Hope*/
 INSERT INTO Operator_Table
-VALUES ('2312',	'4'),
+VALUES
+('2312',	'4'),
 ('2315',	'7'),
 ('2319',	'9'),
 ('2321',	'3')
 
-
 /* Software Table: Harrison Curtis*/
-INSERT INTO Software_Table
-(Software_License_Number, Software_Name)
+INSERT INTO Software_Table (Software_License_Number, Software_Name)
 VALUES
 ('2738918ff2873646', 'Microsoft Word'),
 ('48857845asdgA4', ' Microsoft Outlook'),
@@ -129,26 +123,27 @@ VALUES
 ('ignrierfbis21ngona','Microsoft Outook'),
 
 /* Employee Table: Aaron Homes */
-
-INSERT INTO Employee Table(Caller_ID, Caller_Name, Job, Department, Extension)
-Values('3543','Alex Jones','Customer Assistant','Customer Services','12'),
-      ('6534','Ranalph','Manager','Logistics','12'),
-      ('1232','Rishi','Clerk','Human Resources','12'),
-      ('123','Punjab','Accountant','Finance','12'),
-      ('987','Posco','Foreman','Logistics','12');
+INSERT INTO Employee (Caller_ID, Caller_Name, Job, Department, Extension)
+VALUES
+('3543','Alex Jones','Customer Assistant','Customer Services','12'),
+('6534','Ranalph','Manager','Logistics','12'),
+('1232','Rishi','Clerk','Human Resources','12'),
+('123','Punjab','Accountant','Finance','12'),
+('987','Posco','Foreman','Logistics','12');
 
 /* Equipment Table: Brad Reeder*/
-INSERT INTO Equipment Table (Equipment_Serial_Number, Equipment_Name)
-VALUES ('47392837283628','Epson 23x43')
-	   ('98390820dw33','Sony Mouse')
-	   ('efcf83djj377383','Dell GTX 12')
-	   ('178s8jn38892ndj','Fridge Freezer EX13')
-	   ('efknrn84fn84hf','MacBook pro')
-
+INSERT INTO Equipment (Equipment_Serial_Number, Equipment_Name)
+VALUES
+('47392837283628','Epson 23x43')
+('98390820dw33','Sony Mouse')
+('efcf83djj377383','Dell GTX 12')
+('178s8jn38892ndj','Fridge Freezer EX13')
+('efknrn84fn84hf','MacBook pro')
 
 /* Equipment Problem Table: Harry Salmon*/
-INSERT INTO Equipment Problem (Problem ID, Equipment Serial Number)
-VALUES ('1', '47392837283628'),
+INSERT INTO Equipment_Problem (Problem_ID, Equipment_Serial_Number)
+VALUES
+('1', '47392837283628'),
 ('2', '98390820dw33'),
 ('3', 'efcf83djj377383'),
 ('4', '178s8jn38892ndj'),

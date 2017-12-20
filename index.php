f4e6018cf6e8e1fd58e9353d26d8a57ed31295a8
ollypolly <o.pethick-16@student.lboro.ac.uk>
2017-12-20 23:26:57 +0000

    test db connection

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include_once 'includes/db.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,14 +7,6 @@
 </head>
 <body>
   <?php
-  // Initialise credentials
-  $mysql_host = 'localhost';
-  $mysql_user = 'root';
-  $mysql_pass = 'proteam';
-  $mysql_db = 'sc-web';
-
-  // Create connection
-  $con = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
 
   // Create sql statement and query
   $sql = "SELECT Problem.Problem_ID, Employee.Caller_Name, Problem.Open_Date, Problem.Problem_Type_ID, Problem.State FROM Problem, Employee WHERE Problem.Caller_ID = Employee.Caller_ID;";
@@ -24,12 +17,12 @@
     echo $row['Problem_ID'];
   }
 
-
   // Free result set
   mysqli_free_result($result);
 
   // Close the connection
   mysqli_close($con);
+
   ?>
 </body>
 </html>

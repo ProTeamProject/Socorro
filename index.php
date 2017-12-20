62511370f1b6501fe11a1485c79bf3d17ff9d948
ollypolly <o.pethick-16@student.lboro.ac.uk>
2017-12-20 23:10:37 +0000

    test db connection

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 </head>
 <body>
   <?php
+  // Initialise credentials
   $mysql_host = 'localhost';
   $mysql_user = 'root';
   $mysql_pass = 'proteam';
@@ -14,10 +15,16 @@
   // Create connection
   $con = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
 
-  // Check connection
-  if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+  // Create sql statement and query
+  $sql = 'SELECT Problem.Problem_ID, Employee.Caller_Name, Problem.Open_Date, Problem.Problem_Type_ID, Problem.State FROM Problem, Employee WHERE Problem.Caller_ID = Employee.Caller_ID';
+  $result = mysqli_query($con, $sql);
+
+  // Associative array
+  $row = mysqli_fetch_assoc($result);
+  printf ("%s (%s)\n", $row["Problem_ID"], $row["Caller_Name"], $row["Open_Date"], $row["Problem_Type_ID"], $row["State"]);
+
+  // Free result set
+  mysqli_free_result($result);
 
   // Close the connection
   mysqli_close($con);

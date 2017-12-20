7dcc619aeb51ed320f77d5c69c62204f0c91b47c
ollypolly <o.pethick-16@student.lboro.ac.uk>
2017-12-20 23:22:54 +0000

    test db connection

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+
+// Initialise credentials
+$mysql_host = 'localhost';
+$mysql_user = 'root';
+$mysql_pass = 'proteam';
+$mysql_db = 'sc-web';
+
+// Create connection
+$con = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
+
+?>
--- index.php
+++ index.php
@@ -16,12 +16,14 @@
   $con = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
 
   // Create sql statement and query
-  $sql = 'SELECT Problem.Problem_ID, Employee.Caller_Name, Problem.Open_Date, Problem.Problem_Type_ID, Problem.State FROM Problem, Employee WHERE Problem.Caller_ID = Employee.Caller_ID';
+  $sql = "SELECT Problem.Problem_ID, Employee.Caller_Name, Problem.Open_Date, Problem.Problem_Type_ID, Problem.State FROM Problem, Employee WHERE Problem.Caller_ID = Employee.Caller_ID;";
   $result = mysqli_query($con, $sql);
 
   // Associative array
-  $row = mysqli_fetch_assoc($result);
-  printf ("%s (%s)\n", $row["Problem_ID"], $row["Caller_Name"], $row["Open_Date"], $row["Problem_Type_ID"], $row["State"]);
+  while ($row = mysqli_fetch_assoc($result)) {
+    echo $row['Problem_ID'];
+  }
+
 
   // Free result set
   mysqli_free_result($result);

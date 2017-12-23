6ae053752f7a1ead580bde2e4f572b1fdab75d94
ollypolly <o.pethick-16@student.lboro.ac.uk>
2017-12-23 16:24:40 +0000

    alter db password

--- includes/db.php
+++ includes/db.php
@@ -3,10 +3,15 @@
 // Initialise credentials
 $mysql_host = 'localhost';
 $mysql_user = 'root';
-$mysql_pass = 'proteam';
+$mysql_pass = 'root';
 $mysql_db = 'sc-web';
 
 // Create connection
 $con = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
+
+// Check the connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 
 ?>

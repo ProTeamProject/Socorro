048ad25867251418264028f8ad61380d9efa21ed
JoshuaJamesHope <joshuajameshope@gmail.com>
2018-02-16 18:29:22 +0000

    updated dashboard

--- dashboard/index.php
+++ dashboard/index.php
@@ -7,38 +7,41 @@
 </head>
 <body>
   <?php
-
+  $output = 10;
   // Create sql statement and query
   $sql = "SELECT problem.Problem_ID, employee.Caller_Name, problem.Open_date, problem_status.Status_Date, problem_type.problem_type_name, problem.state
 FROM Problem
 INNER JOIN employee on employee.Caller_ID = problem.Caller_ID
 INNER JOIN problem_status on problem_status.Problem_ID = problem.Problem_ID
 INNER JOIN problem_type on problem_type.Problem_Type_ID = problem.Problem_Type_ID
-WHERE problem.state != 'closed' ORDER BY problem.Open_date desc";
+WHERE problem.state != 'closed' ORDER BY problem.Open_date desc
+LIMIT $output";
   $sql2 = "SELECT problem.Problem_ID, employee.Caller_Name, problem.Open_date, problem_status.Status_Date, problem_type.problem_type_name, problem.state
 FROM Problem
 INNER JOIN employee on employee.Caller_ID = problem.Caller_ID
 INNER JOIN problem_status on problem_status.Problem_ID = problem.Problem_ID
 INNER JOIN problem_type on problem_type.Problem_Type_ID = problem.Problem_Type_ID
 WHERE problem.state != 'closed' ORDER BY problem.Open_date asc";
+
   $result = mysqli_query($con, $sql);
   $result2 = mysqli_query($con, $sql2);
 
   // Associative array
   while ($row = mysqli_fetch_assoc($result)) {
-    echo $row['Problem_ID'] . " " . $row['Caller_name'] . " " . $row['Status_Date'] . " " . $row['problem_type_name'] . " " . $row['State'] . "<br />";
+    echo "<a href='/problem/index.php?id=".$row['Problem_ID']."'</a>" . " " . $row['Problem_ID'] . " " . $row['Caller_Name'] . " " . date('jS F Y H:i', strtotime($row['Open_date'])) . " " . date('jS F Y H:i', strtotime($row['Status_Date'])) . " " . $row['problem_type_name'] . " " . $row['state'] . "<br />";
   }
-  //needs styling to output the two different arrays apart
+  //needs styling to o/utput the two different arrays apart
   while ($row = mysqli_fetch_assoc($result2)) {
-    echo $row['Problem_ID'] . " " . $row['Caller_name'] . " " . $row['Status_Date'] . " " . $row['problem_type_name'] . " " . $row['State'] . "<br />";
+    echo "<a href='/problem/index.php?id=".$row['Problem_ID']."'</a>" . " " . $row['Problem_ID'] . " " . $row['Caller_Name'] . " " . date('jS F Y H:i', strtotime($row['Open_date'])) . " " . date('jS F Y H:i', strtotime($row['Status_Date'])) . " " . $row['problem_type_name'] . " " . $row['state'] . "<br />";
   }
-
   // Free result set
   mysqli_free_result($result);
 
   // Close the connection
   mysqli_close($con);
-
+  function increaseOutput(){
+    $output = $output + 10;
+  }
   ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -5,16 +5,22 @@
   <meta charset="UTF-8">
   <title>Socorro</title>
 </head>
-<body>
+<input type="submit" class="button" name="insert" onlcick = outputIncrease()/>;
   <?php
-
+  $output = 1;
   // Create sql statement and query
+  function outputIncrease(){
+    $output = $output + 1;
+      $stmt->execute();
+  }
+
   $sql = "SELECT problem.Problem_ID, employee.Caller_Name, problem.Open_date, problem_status.Status_Date, problem_type.problem_type_name, problem.state
 FROM Problem
 INNER JOIN employee on employee.Caller_ID = problem.Caller_ID
 INNER JOIN problem_status on problem_status.Problem_ID = problem.Problem_ID
 INNER JOIN problem_type on problem_type.Problem_Type_ID = problem.Problem_Type_ID
-WHERE problem.state != 'closed' ORDER BY problem.Open_date desc";
+WHERE problem.state != 'closed' ORDER BY problem.Open_date desc
+LIMIT :output;";
   $sql2 = "SELECT problem.Problem_ID, employee.Caller_Name, problem.Open_date, problem_status.Status_Date, problem_type.problem_type_name, problem.state
 FROM Problem
 INNER JOIN employee on employee.Caller_ID = problem.Caller_ID
@@ -22,23 +28,27 @@ INNER JOIN problem_status on problem_status.Problem_ID = problem.Problem_ID
 INNER JOIN problem_type on problem_type.Problem_Type_ID = problem.Problem_Type_ID
 WHERE problem.state != 'closed' ORDER BY problem.Open_date asc";
 
-  $result = mysqli_query($con, $sql);
-  $result2 = mysqli_query($con, $sql2);
+  $stmt = $con->prepare($sql);
+  $stmt->bindParam(':output', $output);
+$stmt->execute();
+  $stmt2 = $con->prepare($sql2);
+  $stmt2->execute();
+
+
 
   // Associative array
-  while ($row = mysqli_fetch_assoc($result)) {
-    echo "<a href='/problem/index.php?id=".$row['Problem_ID']."'</a>" . " " . $row['Problem_ID'] . " " . $row['Caller_Name'] . " " . date('jS F Y H:i', strtotime($row['Open_date'])) . " " . date('jS F Y H:i', strtotime($row['Status_Date'])) . " " . $row['problem_type_name'] . " " . $row['state'] . "<br />";
+  while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    echo "<a href='/problem/index.php?id=".$row['Problem_ID']."'>" . " " . $row['Problem_ID'] . " " . $row['Caller_Name'] . " " . date('jS F Y H:i', strtotime($row['Open_date'])) . " " . date('jS F Y H:i', strtotime($row['Status_Date'])) . " " . $row['problem_type_name'] . " " . $row['state'] . "></a><br />";
   }
   //needs styling to o/utput the two different arrays apart
-  while ($row = mysqli_fetch_assoc($result2)) {
-    echo "<a href='/problem/index.php?id=".$row['Problem_ID']."'</a>" . " " . $row['Problem_ID'] . " " . $row['Caller_Name'] . " " . date('jS F Y H:i', strtotime($row['Open_date'])) . " " . date('jS F Y H:i', strtotime($row['Status_Date'])) . " " . $row['problem_type_name'] . " " . $row['state'] . "<br />";
+  while ($row = $stmt2->fetch(PDO::FETCH_ASSOC)) {
+    echo "<a href='/problem/index.php?id=".$row['Problem_ID']."'>" . " " . $row['Problem_ID'] . " " . $row['Caller_Name'] . " " . date('jS F Y H:i', strtotime($row['Open_date'])) . " " . date('jS F Y H:i', strtotime($row['Status_Date'])) . " " . $row['problem_type_name'] . " " . $row['state'] . "</a><br />";
   }
-  // Free result set
-  mysqli_free_result($result);
 
-  // Close the connection
-  mysqli_close($con);
 
-  ?>
+  echo $output;
+?>
+
+
 </body>
 </html>
